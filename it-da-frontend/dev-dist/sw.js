ab03f97f28db8b24478f3b2fc3785f910e5bbfdc
ui jin shin <ujblackjack@gmail.com>
2026-01-30 09:56:08 +0900

    no message

--- it-da-frontend/dev-dist/sw.js
+++ it-da-frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-237f2c1f'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.eb00tdfpsuo"
+    "revision": "0.s55ukuhkru"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
